15064a72c848499efcd816c1826ffe18c2de877c
Mourad EL CADI <mouradface1998@gmail.com>
2021-04-07 17:34:42 +0100

    .

--- my-plug.php
+++ my-plug.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Plugin Name:       My Basics Plugin
+ * Plugin Name:       ** My Plugin
  * Description:       Handle the basics with this plugin.
  * Plugin URI:        https://mourad.com/plugins/the-basics/
  * Version:           1.0.0
@@ -14,3 +14,10 @@
  * Text Domain:       my-basics-plugin
  * Domain Path:       /languages
  */
+
+
+add_action('woocommerce_cart_calculate_fees', function(){
+    global $woocommerce;
+
+    $woocommerce->cart->add_fee(__('Packaging fees', 'woocommerce'), 5);
+});
