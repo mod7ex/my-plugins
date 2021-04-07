9e35ee216f21fb390bd0593711d6e6618895e864
Mourad EL CADI <mouradface1998@gmail.com>
2021-04-07 17:04:21 +0100

    basic conf

---
+++ my-plug.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name:       My Basics Plugin
+ * Description:       Handle the basics with this plugin.
+ * Plugin URI:        https://mourad.com/plugins/the-basics/
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mourad EL CADI
+ * Author URI:        https://mourad.elcadi.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.mourad.org/licenses/gpl-2.0.html
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
